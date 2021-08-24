ed8c2b45f1dbc9565d489464bcd6168c8ae7fe17
Chittadeep Biswas <mailchittadeep@gmail.com>
2021-08-24 11:54:53 +0530

    first commit

---
+++ android/app/src/main/kotlin/com/example/e_mailer/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.e_mailer
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,71 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_signin_button/flutter_signin_button.dart';
+import 'package:mailer/mailer.dart';
+import 'package:mailer/smtp_server/gmail.dart';
+import 'package:fluttertoast/fluttertoast.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: Center(
+            child: SignInButton(
+          Buttons.Google,
+          onPressed: () async {
+            sendMessage();
+          },
+        )),
+      ),
+    );
+  }
+
+  Future<void> sendMessage() async {
+    String username = 'mailchittadeep@gmail.com';
+    String password = 'forgottenpassword';
+
+    final smtpServer = gmail(username, password);
+    // Use the SmtpServer class to configure an SMTP server:
+    // final smtpServer = SmtpServer('smtp.domain.com');
+    // See the named arguments of SmtpServer for further configuration
+    // options.
+
+    // Create our message.
+    final message = Message()
+      ..from = Address(username, 'e_mailer')
+      ..recipients.add('mailchittadeep@gmail.com')
+      ..ccRecipients.addAll(['destCc1@example.com', 'destCc2@example.com'])
+      ..bccRecipients.add(Address('bccAddress@example.com'))
+      ..subject = 'Test Dart Mailer library :: 😀 :: ${DateTime.now()}'
+      ..text = 'This is the plain text.\nThis is line 2 of the text part.'
+      ..html = "<h1>Test</h1>\n<p>Hey! Here's some HTML content</p>";
+
+    try {
+      final sendReport = await send(message, smtpServer);
+      print('Message sent: ' + sendReport.toString());
+    } on MailerException catch (e) {
+      print('Message not sent.');
+      for (var p in e.problems) {
+        print('Problem: ${p.code}: ${p.msg}');
+      }
+    }
+    var connection = PersistentConnection(smtpServer);
+
+    await connection.send(message);
+
+    Fluttertoast.showToast(
+        msg: "This is Center Short Toast",
+        toastLength: Toast.LENGTH_SHORT,
+        gravity: ToastGravity.CENTER,
+        timeInSecForIosWeb: 1,
+        backgroundColor: Colors.red,
+        textColor: Colors.white,
+        fontSize: 16.0
+    );
+  }
+}
